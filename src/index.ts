a825ffbf927e158e4e1f0dbc33e33d66f828ca22
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-02-20 19:48:30 +0000

    Started code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const { writeFile, readdir, readFile } = require("fs").promises;
+
+const configFiles: any = {};
+const configFolderPath = path.resolve(__dirname, "config");
+
+async () => {
+	const files = await readdir(configFolderPath).catch(console.log);
+
+	for (let i of files) {
+		const gitignoreName = i.split(".")[1];
+		configFiles[gitignoreName] = path.join(configFolderPath, i);
+	}
+
+	const { language } = await inquirer.prompt([
+		{
+			type: "list",
+			message:
+				"Which Language do you want to generate the .gitignore for?",
+			name: "language",
+			choices: Object.keys(configFiles),
+		},
+	]);
+};
